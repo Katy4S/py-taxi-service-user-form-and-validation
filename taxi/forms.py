fb353b941d84e2658217766f8e974e6a8fc0abd8
K. Petrychenko <catpetrychenko@gmail.com>
2024-10-16 20:30:40 +0300

    Solution

--- taxi/forms.py
+++ taxi/forms.py
@@ -4,13 +4,15 @@ from django.contrib.auth import get_user_model
 import re
 from .models import Car, Driver
 
+from django.contrib.auth.forms import UserCreationForm
+
 User = get_user_model()
 
 
-class DriverCreationForm(forms.ModelForm):
+class DriverCreationForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ["username", "license_number", "email"]
+        fields = ("username", "first_name", "last_name", "email")
 
     def clean_license_number(self):
         license_number = self.cleaned_data["license_number"]
